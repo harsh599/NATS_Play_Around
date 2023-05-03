59e602dc37fca1f9c720556ecba52492bd3320c2
Harsh Srivastava <harshsrivastava599@gmail.com>
2023-05-02 23:58:26 -0300

    Fix
    Working Nats ws client and deno backend just run from the configuration file
    cmd: nats-server -c nats.conf
    that's it.

--- NATS_VUE_WS/src/components/NatsSubscription.vue
+++ NATS_VUE_WS/src/components/NatsSubscription.vue
@@ -10,32 +10,28 @@
 <script setup lang="ts">
 import { connect, StringCodec } from "nats.ws";
 import { onMounted, reactive, ref } from 'vue';
-
-
 const messages = ref<any>([]);
-const sc = StringCodec();
+// const sc = StringCodec();
 const nc = ref<any>();
 const sub = ref<any>();
-onMounted(async()=>{
-    try{
-        nc.value = await connect({ servers: "localhost:4222" });
-        sub.value = nc.value.subscribe("hello");
-        for await (const m of sub) {
-            const message = sc.decode(m.data);
-            messages.value.push(`[${sub.value.getProcessed()}]: ${message}`);
-        }
 
-        nc.value.publish("hello", sc.encode("world"));
-        nc.value.publish("hello", sc.encode("again"));
-    }catch(e){
+onMounted(async()=>{
+    try {
+        nc.value = await connect({ servers: "ws://localhost:8080" });
+        sub.value = nc.value.subscribe("hello");
+        const iterator = sub.value[Symbol.asyncIterator]();
+        while (true) {
+            const message = await iterator.next();
+            if (message.done) break;
+            console.log(message.value.data);
+            messages.value.push(`[${sub.value.getProcessed()}]: ${message.value.data}`);
+        }
+        nc.value.publish("hello", "world");
+        nc.value.publish("hello", "again");
+    } catch(e){
         console.error(e);
     }finally{
         await nc.value.drain();
     }
-   
-
 });
-
-
-
 </script>
